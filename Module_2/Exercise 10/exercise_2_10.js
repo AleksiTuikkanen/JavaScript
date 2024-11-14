b4ca845978a303276a6aec641ed3655572ae211e
AleksiTuikkanen <aleksi.tuikkanen@metropolia.fi>
2024-11-14 13:38:52 +0200

    javascript tehtävät

--- Module_2/Exercise 10/exercise_2_10.js
+++ Module_2/Exercise 10/exercise_2_10.js
@@ -1,18 +1,34 @@
 'use strict';
+const election = [];
 
-function even(array) {
-  const new_array = [];
-  for (let num of array) {
-    if (num % 2 === 0)
-    {
-      new_array.push(num);
-    }
-  }
-  return (new_array);
+const number_of_candidates = Number(prompt('How many candidates?'));
+
+for (let i = 1; i <= number_of_candidates; i++) {
+  const candidate_name = prompt('Name for candidate number:' + i);
+  const candidate = {name: candidate_name, votes: 0};
+  election.push(candidate);
+
 }
 
-const first_array = [1, 2, 3, 4, 5, 5, 6, 7, 8, 11, 9, 41, 45, 79, 80];
-const second_array = even(first_array);
+const number_of_voters = Number(prompt('How many voters?'));
 
-console.log(first_array);
-console.log(second_array);
+for (let i = 1; i <= number_of_voters; i++) {
+  const vote_for_who = prompt('vote for which candidate');
+  const candidate = election.find(c => c.name === vote_for_who);
+  if (candidate) {
+    candidate.votes += 1;
+  } else {
+    console.log('candidate not found ' + vote_for_who);
+  }
+}
+
+let winner = election[0];  // Start by assuming the first candidate is the winner
+
+for (let i = 1; i < election.length; i++) {
+  if (election[i].votes > winner.votes) {
+    winner = election[i];  // Update winner if the current candidate has more votes
+  }
+}
+
+console.log(`The winner is ${winner.name} with ${winner.votes} votes.`);
+console.log(election);
